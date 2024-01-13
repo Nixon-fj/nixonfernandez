f5206da1665e70ccb7ce08ab6c6ebf8d1c1fd093
Nixon <71091247+Nixon-fj@users.noreply.github.com>
2024-01-13 02:54:20 -1200

    actualizacion de boton ver curriculum

--- src/components/resume/Presentation.tsx
+++ src/components/resume/Presentation.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { saveAs } from 'file-saver';
 import { useTranslation } from 'react-i18next';
 import bg from '../../assets/images/pattern.jpg';
 
@@ -17,16 +16,9 @@ const Presentation: React.FC = () => {
     const presentation: presentationText = t('presentation', { returnObjects: true });
     const pdfUrl = 'https://firebasestorage.googleapis.com/v0/b/nixon-fernandez-jimenez.appspot.com/o/Nixon%20Fern%C3%A1ndez%20Jim%C3%A9nez.pdf?alt=media&token=69681893-0480-41d8-b59b-45040345d7fb';
 
-    const handleDownloadPdf = async () => {
-        try {
-            const response = await fetch(pdfUrl);
-            const blob = await response.blob();
-
-            saveAs(blob, 'devNixonFernandezJimenez.pdf');
-
-        } catch (error) {
-            console.error('Error al descargar el PDF:', error);
-        }
+    const handleOpenPdf = () => {
+        // Abrir el PDF en una nueva pestaña
+        window.open(pdfUrl, '_blank');
     };
 
     return <div className='presentation'>
@@ -37,7 +29,7 @@ const Presentation: React.FC = () => {
         </div>
         <div className="presentation_text">
             <p>{presentation.aboutMe}</p>
-            <button onClick={handleDownloadPdf} className='btn'>{presentation.downloadResume}</button>
+            <button onClick={handleOpenPdf} className='btn'>{presentation.downloadResume}</button>
         </div>
     </div>;
 };
